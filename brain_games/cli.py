41c069c454918610d56f25569605ecf7742c102d
Юлия Хомякова <yulia.khomiakova@gmail.com>
2025-10-29 11:54:59 +0100

    Исправила  i на большое I и добавила пробел после ?

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,5 +3,5 @@ import prompt
 
 def welcome_user():
     print("Welcome to the Brain Games!")
-    name = prompt.string("May i have your name?")
+    name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
